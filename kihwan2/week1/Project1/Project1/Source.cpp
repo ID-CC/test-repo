0d99e85618dfc33c99f9938d195a8ba3a87bd736
Kihwan Nam <magmacoffees@gmail.com>
2022-06-29 21:16:56 +0900

    bug fix

--- kihwan2/week1/Project1/Project1/Source.cpp
+++ kihwan2/week1/Project1/Project1/Source.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main() {
 	cout << "Hello World" << endl;
 	cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
 	return 0;
 }
